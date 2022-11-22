af9d0d6e16bd88e718fb0546aa6cdef20477a010
조송하 <hai81532@gmail.com>
2022-11-22 17:27:03 +0900

    Add project files.

---
+++ App.xaml.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include "App.xaml.g.h"
+
+namespace winrt::Progress_Bar_LAB::implementation
+{
+    struct App : AppT<App>
+    {
+        App();
+
+        void OnLaunched(Microsoft::UI::Xaml::LaunchActivatedEventArgs const&);
+
+    private:
+        winrt::Microsoft::UI::Xaml::Window window{ nullptr };
+    };
+}
